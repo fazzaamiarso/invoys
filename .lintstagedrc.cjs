1d4d7a190e5fd15c7ba51799734edb21ac98d6e0
fazzaamiarso <fazzaamiarso@gmail.com>
2022-10-10 04:56:13 +0700

    fix(lint): fix lint-staged to be able to work with next.js

--- .lintstagedrc.cjs
+++ .lintstagedrc.cjs
@@ -6,5 +6,5 @@ const buildEslintCommand = filenames =>
     .join(' --file ')}`;
 
 module.exports = {
-  '*.{js,jsx,ts,tsx}': [buildEslintCommand],
+  'src/**/*.{js,jsx,ts,tsx}': [buildEslintCommand, 'prettier --write'],
 };
