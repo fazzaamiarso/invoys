a740df0a9d69fe9adc24b30432a72210bb7ad159
fazzaamiarso <fazzaamiarso@gmail.com>
2022-11-01 00:03:05 +0700

    ci: fix cypress test

--- cypress.config.ts
+++ cypress.config.ts
@@ -6,6 +6,7 @@ export default defineConfig({
     video: false,
     baseUrl: 'http://localhost:8080',
     screenshotOnRunFailure: false,
+    defaultCommandTimeout: 7000,
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
