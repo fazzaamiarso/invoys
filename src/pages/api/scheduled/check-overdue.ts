256001f7fc56f80f73b383c4e4fd5ced46c7c1c8
fazzaamiarso <fazzaamiarso@gmail.com>
2022-11-17 23:51:30 +0700

    fix: add webhook secret checking tos check-overdue

--- src/pages/api/scheduled/check-overdue.ts
+++ src/pages/api/scheduled/check-overdue.ts
@@ -6,11 +6,22 @@ export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
+  // set this in app hosting provider
+  const { APP_CRON_KEY } = process.env;
+  // set this webhook caller, in our case it's courier automation api
+  const ACTION_KEY = req.headers.authorization?.split(' ')[1];
+
+  if (!ACTION_KEY) res.status(401);
+
   const caller = appRouter.createCaller({} as any);
 
   try {
-    await caller.invoice.batchUpdateOverdues();
-    res.status(200).json({ success: 'true' });
+    if (ACTION_KEY === APP_CRON_KEY) {
+      await caller.invoice.batchUpdateOverdues();
+      res.status(200).json({ success: 'true' });
+    } else {
+      res.status(401);
+    }
   } catch (err) {
     res.status(500);
   }
